eb79a7195571e86d02909286aaf12214cbb9c5f4
domiluk <151018843+domiluk@users.noreply.github.com>
2023-11-18 10:38:30 +0100

    Colors and blending modes

--- shooting-gallery/main.lua
+++ shooting-gallery/main.lua
@@ -5,9 +5,21 @@ function love.update(dt)
 end
 
 function love.draw()
-    love.graphics.rectangle("fill", 100, 50, 150, 100)
-    love.graphics.rectangle("line", 300, 100, 200, 100)
-    love.graphics.circle("fill", 300, 200, 60)
+    love.graphics.setBlendMode("alpha")
+    love.graphics.setColor(0.9, 0.6, 0.5)
+    love.graphics.rectangle("line", 100, 50, 150, 100)
+    love.graphics.rectangle("fill", 200, 200, 200, 200)
+    love.graphics.rectangle("fill", 300, 300, 200, 200)
 
+    love.graphics.setBlendMode("add")
+    love.graphics.setColor(1, 0, 0, 0.9)
+    love.graphics.circle("fill", 500, 360, 100)
+    love.graphics.setColor(0, 1, 0, 0.9)
+    love.graphics.circle("fill", 450, 450, 100)
+    love.graphics.setColor(0, 0, 1, 0.9)
+    love.graphics.circle("fill", 550, 450, 100)
+
+    love.graphics.setBlendMode("alpha")
+    love.graphics.setColor(1, 1, 1)
     love.graphics.print(love.timer.getFPS())
 end
