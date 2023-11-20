3a9717bf6a2bfc7cbe19dce89c4ce2fab105cebb
domiluk <151018843+domiluk@users.noreply.github.com>
2023-11-20 12:12:05 +0100

    Bullets

--- top-down-shooter/main.lua
+++ top-down-shooter/main.lua
@@ -1,3 +1,5 @@
+LMB = 1
+
 function love.load()
     sprites = {}
     sprites.background = love.graphics.newImage("sprites/background.png")
@@ -11,6 +13,7 @@ function love.load()
     player.speed = 180
 
     zombies = {}
+    bullets = {}
 end
 
 function love.keypressed(key)
@@ -18,6 +21,12 @@ function love.keypressed(key)
         love.event.quit()
     elseif key == "space" then
         spawnZombie()
+    end
+end
+
+function love.mousepressed(x, y, button)
+    if button == LMB then
+        spawnBullet()
     end
 end
 
@@ -42,6 +51,18 @@ function love.update(dt)
             zombies = {}
         end
     end
+
+    for _, bullet in ipairs(bullets) do
+        bullet.x = bullet.x + math.cos(bullet.direction) * bullet.speed * dt
+        bullet.y = bullet.y + math.sin(bullet.direction) * bullet.speed * dt
+    end
+
+    for i = #bullets, 1, -1 do
+        local bullet = bullets[i]
+        if bullet.x < 0 or bullet.y < 0 or bullet.x > love.graphics.getWidth() or bullet.y > love.graphics.getHeight() then
+            table.remove(bullets, i)
+        end
+    end
 end
 
 function love.draw()
@@ -53,6 +74,11 @@ function love.draw()
     for _, zombie in ipairs(zombies) do
         love.graphics.draw(sprites.zombie, zombie.x, zombie.y, zombieFacingPlayerAngle(zombie), nil, nil,
             sprites.zombie:getWidth() / 2, sprites.zombie:getHeight() / 2)
+    end
+
+    for _, bullet in ipairs(bullets) do
+        love.graphics.draw(sprites.bullet, bullet.x, bullet.y, nil, 0.5, nil,
+            sprites.bullet:getWidth() / 2, sprites.bullet:getHeight() / 2)
     end
 end
 
@@ -70,6 +96,15 @@ function spawnZombie()
     zombie.y = math.random(0, love.graphics.getHeight())
     zombie.speed = 100
     table.insert(zombies, zombie)
+end
+
+function spawnBullet()
+    local bullet = {}
+    bullet.x = player.x
+    bullet.y = player.y
+    bullet.speed = 500
+    bullet.direction = playerFacingMouseAngle()
+    table.insert(bullets, bullet)
 end
 
 function distanceBetween(x1, y1, x2, y2)
