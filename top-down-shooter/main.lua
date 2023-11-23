2d1df566e4a113f693215d4c057a01b99d42baa9
domiluk <151018843+domiluk@users.noreply.github.com>
2023-11-23 22:06:35 +0100

    More refactoring
    Codealong done at this point

--- top-down-shooter/main.lua
+++ top-down-shooter/main.lua
@@ -25,32 +25,32 @@ function love.load()
 
     gameState = MAIN_MENU
     score = 0
-    maxTime = 2
-    timer = maxTime
+    timeBetweenZombieSpawns = 2
+    zombieSpawnTimer = timeBetweenZombieSpawns
 end
 
 function love.keypressed(key)
     if key == "escape" then
         love.event.quit()
-    elseif key == "space" then
-        spawnZombie()
     end
 end
 
 function love.mousepressed(x, y, button)
-    if button == LMB and gameState == IN_GAME then
-        spawnBullet()
-    elseif button == LMB and gameState == MAIN_MENU then
-        gameState = IN_GAME
-        score = 0
-        maxTime = 2
-        timer = maxTime
+    if button == LMB then
+        if gameState == IN_GAME then
+            spawnBullet()
+        elseif gameState == MAIN_MENU then
+            gameState = IN_GAME
+            score = 0
+            timeBetweenZombieSpawns = 2
+            zombieSpawnTimer = timeBetweenZombieSpawns
+        end
     end
 end
 
 function love.update(dt)
-    -- player movement
     if gameState == IN_GAME then
+        -- player movement
         if love.keyboard.isDown("w") or love.keyboard.isDown("up") then
             player.y = math.max(0, player.y - player.speed * dt)
         end
@@ -63,60 +63,49 @@ function love.update(dt)
         if love.keyboard.isDown("d") or love.keyboard.isDown("right") then
             player.x = math.min(love.graphics.getWidth(), player.x + player.speed * dt)
         end
-    end
 
-    -- move zombies
-    for _, zombie in ipairs(zombies) do
-        zombie.x = zombie.x + math.cos(zombieFacingPlayerAngle(zombie)) * zombie.speed * dt
-        zombie.y = zombie.y + math.sin(zombieFacingPlayerAngle(zombie)) * zombie.speed * dt
-        if zombieTouchedThePlayer(zombie) then
-            zombies = {}
-            gameState = MAIN_MENU
-            player.x = love.graphics.getWidth() / 2
-            player.y = love.graphics.getHeight() / 2
-        end
-    end
-
-    -- move bullets
-    for _, bullet in ipairs(bullets) do
-        bullet.x = bullet.x + math.cos(bullet.direction) * bullet.speed * dt
-        bullet.y = bullet.y + math.sin(bullet.direction) * bullet.speed * dt
-    end
-
-    -- bullet-zombie collision detection
-    for _, zombie in ipairs(zombies) do
-        for _, bullet in ipairs(bullets) do
-            if bulletHitAZombie(bullet, zombie) then
-                zombie.dead = true
-                bullet.dead = true
-                score = score + 1
+        -- move zombies
+        for _, zombie in ipairs(zombies) do
+            zombie.x = zombie.x + math.cos(zombieFacingPlayerAngle(zombie)) * zombie.speed * dt
+            zombie.y = zombie.y + math.sin(zombieFacingPlayerAngle(zombie)) * zombie.speed * dt
+            -- collision detection with the player
+            if zombieTouchedThePlayer(zombie) then
+                zombies = {}
+                bullets = {}
+                gameState = MAIN_MENU
+                player.x = love.graphics.getWidth() / 2
+                player.y = love.graphics.getHeight() / 2
             end
         end
-    end
 
-    -- remove dead zombies
-    for i = #zombies, 1, -1 do
-        local zombie = zombies[i]
-        if zombie.dead then
-            table.remove(zombies, i)
+        -- move bullets
+        for _, bullet in ipairs(bullets) do
+            bullet.x = bullet.x + math.cos(bullet.direction) * bullet.speed * dt
+            bullet.y = bullet.y + math.sin(bullet.direction) * bullet.speed * dt
+            -- collision detection with zombies
+            for _, zombie in ipairs(zombies) do
+                if bulletHitAZombie(bullet, zombie) then
+                    zombie.dead = true
+                    bullet.dead = true
+                    score = score + 1
+                end
+            end
+            -- collision detection with sides of the screen
+            if bullet.x < 0 or bullet.y < 0 or bullet.x > love.graphics.getWidth() or bullet.y > love.graphics.getHeight() then
+                bullet.dead = true
+            end
         end
-    end
 
-    -- remove dead bullets
-    for i = #bullets, 1, -1 do
-        local bullet = bullets[i]
-        if bullet.dead or bullet.x < 0 or bullet.y < 0 or bullet.x > love.graphics.getWidth() or bullet.y > love.graphics.getHeight() then
-            table.remove(bullets, i)
-        end
-    end
+        -- clean up dead objects
+        removeDead(zombies)
+        removeDead(bullets)
 
-    -- update spawn timer
-    if gameState == IN_GAME then
-        timer = timer - dt
-        if timer <= 0 then
+        -- update spawn timer
+        zombieSpawnTimer = zombieSpawnTimer - dt
+        if zombieSpawnTimer <= 0 then
             spawnZombie()
-            maxTime = 0.95 * maxTime
-            timer = maxTime
+            timeBetweenZombieSpawns = 0.95 * timeBetweenZombieSpawns
+            zombieSpawnTimer = timeBetweenZombieSpawns
         end
     end
 end
@@ -194,4 +183,13 @@ end
 
 function bulletHitAZombie(bullet, zombie)
     return distanceBetween(zombie.x, zombie.y, bullet.x, bullet.y) < 20
+end
+
+function removeDead(gameObjectArray)
+    for i = #gameObjectArray, 1, -1 do
+        local item = gameObjectArray[i]
+        if item.dead then
+            table.remove(gameObjectArray, i)
+        end
+    end
 end
